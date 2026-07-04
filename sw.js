4cbb9397f72d9249fe572985c5efe65464530c1a
enislucas <enislucas12@gmail.com>
2026-07-04 03:18:33 +0200

    Spațiere traseu lecții

--- sw.js
+++ sw.js
@@ -5,15 +5,15 @@
 //    stale-while-revalidate: merg offline, se împrospătează pe net
 // La versiune nouă: deploy.sh schimbă VERSION → clientul primește banner "Actualizează".
 
-const VERSION = 'ezr-202607040316';
+const VERSION = 'ezr-202607040318';
 const DATA_CACHE = 'ezr-data-v1';
 const SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './css/base.css?v=202607040316',
-  './css/themes.css?v=202607040316',
-  './js/main.js?v=202607040316',
+  './css/base.css?v=202607040318',
+  './css/themes.css?v=202607040318',
+  './js/main.js?v=202607040318',
   './js/state.js',
   './js/ui.js',
   './js/engine.js',
