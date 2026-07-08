3d0f0006a1c01bbf7aa9c097a43d61ca6f6fcae0
enislucas <enislucas12@gmail.com>
2026-07-08 12:58:04 +0200

    Coroane Expert si Maestru

--- sw.js
+++ sw.js
@@ -5,15 +5,15 @@
 //    stale-while-revalidate: merg offline, se împrospătează pe net
 // La versiune nouă: deploy.sh schimbă VERSION → clientul primește banner "Actualizează".
 
-const VERSION = 'ezr-202607081235';
+const VERSION = 'ezr-202607081258';
 const DATA_CACHE = 'ezr-data-v1';
 const SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './css/base.css?v=202607081235',
-  './css/themes.css?v=202607081235',
-  './js/main.js?v=202607081235',
+  './css/base.css?v=202607081258',
+  './css/themes.css?v=202607081258',
+  './js/main.js?v=202607081258',
   './js/state.js',
   './js/ui.js',
   './js/engine.js',
